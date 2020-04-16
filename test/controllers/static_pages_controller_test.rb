8687ac1afa9ae50ef93cc807eecaf41fbcc2867b
hsiaoshihying <hsiao.shihying@moneyforward.co.jp>
2020-04-16 17:36:01 +0900

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
   end
 
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "Home | Coronapreventer"
   end
